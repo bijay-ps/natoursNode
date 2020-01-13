59477cb15ef2aab03160efa2a2f90a80bbbe96d8
bijay_ps <bijay.prakash7@yahoo.in>
2020-01-13 08:54:41 +0530

    added comments

--- utils/appError.js
+++ utils/appError.js
@@ -5,6 +5,10 @@ class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
 
+    // here 'this' refers to current object and
+    // 'this.constructor' refers to current class, AppError
+    // so, this way when a new object is created and constructor function is called
+    // then that function call is not gonna appear in stack trace.
     Error.captureStackTrace(this, this.constructor);
   }
 }
