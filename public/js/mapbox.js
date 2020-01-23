e4c93bd879129838c7e7b6643ddc182278d43b53
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-01-23 06:39:06 +0530

    added bundler

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,48 +1,48 @@
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiYmlqYXktcHMiLCJhIjoiY2s1bW42ODZ1MDk2dzNsbDV4NzFhbGJvdSJ9.VFo5OpuXsF1-_ve6yX2x7Q';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYmlqYXktcHMiLCJhIjoiY2s1bW42ODZ1MDk2dzNsbDV4NzFhbGJvdSJ9.VFo5OpuXsF1-_ve6yX2x7Q';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/bijay-ps/ck5mnc4yd3g0d1ijyuzgrlk4k',
+    scrollZoom: false
+    /*center: [-118.11349134, 34.111745],
+    zoom: 10,
+    interactive: false*/
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/bijay-ps/ck5mnc4yd3g0d1ijyuzgrlk4k',
-  scrollZoom: false
-  /*center: [-118.11349134, 34.111745],
-  zoom: 10,
-  interactive: false*/
-});
+  const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-const bounds = new mapboxgl.LngLatBounds();
-locations.forEach(loc => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    // Extend map bounds to include current locations
+    bounds.extend(loc.coordinates);
+  });
 
-  // Extend map bounds to include current locations
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
