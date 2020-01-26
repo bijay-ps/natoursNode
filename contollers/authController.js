0482908272ca8de860539bd8407c243d06af07b1
BIJAY SINGH <bijay.prakash7@yahoo.in>
2020-01-26 11:15:02 +0530

    added user account page

--- contollers/authController.js
+++ contollers/authController.js
@@ -126,6 +126,7 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // Grant access to protected route
   req.user = freshUser;
+  res.locals.user = freshUser;
   next();
 });
 
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const viewControllers = require('../contollers/viewsController');
 const authController = require('../contollers/authController');
 
-router.use(authController.isLoggedIn);
-
-router.get('/', viewControllers.getOverview);
-router.get('/tour/:slug', viewControllers.getTour);
-router.get('/login', viewControllers.getLoginForm);
+router.get('/', authController.isLoggedIn, viewControllers.getOverview);
+router.get('/tour/:slug', authController.isLoggedIn, viewControllers.getTour);
+router.get('/login', authController.isLoggedIn, viewControllers.getLoginForm);
+router.get('/me', authController.protect, viewControllers.getAccount);
 
 module.exports = router;
