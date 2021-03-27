4ce4c4efd8a531d926b5ddaa936ccbaf03d54737
bijay_ps <bijay.singh.ext@zeiss.com>
2021-03-27 10:30:58 +0530

    checkpoint commit

--- controllers/toursControllers.js
+++ controllers/toursControllers.js
@@ -1,73 +1,102 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.getAllTours = async (req, res) => {
+  try {
+    console.log(req.query);
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-exports.checkID = (res, req, next, val) => {
-  console.log(req.req.headers);
-  if (+val > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryObj));
+
+    const tours = await query;
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err,
     });
   }
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Name / price missing',
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'failed',
+      message: err,
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const tour = tours.find((t) => t.id === +req.params.id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-exports.updateTour = (req, res) => {};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,64 +1,48 @@
-const mongoose = require('mongoose');
-const path = require('path');
-const dotEnv = require('dotenv');
-dotEnv.config({ path: './config.env' });
 const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
-const Review = require('./../../models/reviewModal');
-const User = require('./../../models/userModel');
 
-const port = process.env.PORT || 3000;
-
+dotenv.config({ path: '../../config.env' });
+console.log('process.env.DB: ', process.env.DB);
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
-// const DB = process.env.DB_LOCAL;
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('DB connection successful');
+    console.log('DB connection successful!');
   });
 
 // READ JSON FILE
-// const tours = JSON.parse(fs.readFileSync(path.join(__dirname, 'tours-simple.json'), 'utf-8'));
 const tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'tours.json'), 'utf-8')
-);
-const users = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'users.json'), 'utf-8')
-);
-const reviews = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'reviews.json'), 'utf-8')
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
-    await User.create(users, { validateBeforeSave: false });
-    await Review.create(reviews);
     console.log('Data successfully loaded!');
-  } catch (e) {
-    console.log(e);
+    process.exit();
+  } catch (err) {
+    console.log(err);
   }
-  process.exit(0);
 };
 
-// DELETE DATA FROM DB
+// DELETE ALL DATA FROM DB
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    await User.deleteMany();
-    await Review.deleteMany();
     console.log('Data successfully deleted!');
-  } catch (e) {
-    console.log(e);
+    process.exit();
+  } catch (err) {
+    console.log(err);
   }
-  process.exit(0);
 };
 
 if (process.argv[2] === '--import') {
@@ -66,3 +50,5 @@ if (process.argv[2] === '--import') {
 } else if (process.argv[2] === '--delete') {
   deleteData();
 }
+
+console.log(process.argv);
